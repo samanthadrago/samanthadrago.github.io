8f035f1492e606afb9c5fa726d65712f22290832
samanthadrago <samantha@datadoghq.com>
2017-04-16 11:51:53 -0400

    ALL THE THINGS

--- app.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    function showMap(e) {
-        e.preventDefault;
-        document.getElementById('map').className = 'show';
-    };
-
-    document.getElementById('address').onclick = showMap;
-})()
